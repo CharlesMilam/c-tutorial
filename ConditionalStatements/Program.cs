81f2f4804f09b36d25ce0ca36462e5bfb0fb10b8
Milam <charles.milam@gmail.com>
2015-05-19 22:45:26 -0500

    Modify ConditionalStatements, add main class to choose between running if statement example or switch statement example. Add switch statement class to namespace.

--- ConditionalStatements/Program.cs
+++ ConditionalStatements/Program.cs
@@ -2,9 +2,28 @@
 
 namespace ConditionalStatements
 {
+  class MainClass
+  {
+    public static void Main (string[] args)
+    {
+      Console.WriteLine("If or Switch");
+      string input = Console.ReadLine();
+
+      switch (input.ToLower())
+      {
+      case "if":
+        IfStatement.MainIf();
+        break;
+      case "switch":
+        SwitchStatement.MainSwitch();
+        break;
+      }
+    }
+  }
+
 	class IfStatement
 	{
-		public static void Main (string[] args)
+		public static void MainIf ()
 		{
       int number;
 
@@ -17,4 +36,27 @@ namespace ConditionalStatements
         Console.WriteLine("The number should be between 0 and 10, inclusive.");
 		}
 	}
+
+  class SwitchStatement
+  {
+    public static void MainSwitch ()
+    {
+      Console.WriteLine ("So...C#? yes/no/maybe");
+      string input = Console.ReadLine();
+
+      switch (input.ToLower())
+      {
+      case "yes":
+      case "maybe":
+        Console.WriteLine("We shall see...won't we.");
+        break;
+      case "no":
+        Console.WriteLine("Too bad...but perhaps a wise decision.");
+        break;
+      default:
+        Console.WriteLine ("Huh?");
+        break;
+      }
+    }
+  }
 }
