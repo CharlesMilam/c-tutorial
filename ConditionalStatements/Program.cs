a25e853eb19f60f07351e4a1f35a14d1fb09d1ba
Milam <charles.milam@gmail.com>
2015-05-19 22:10:41 -0500

    Initial commit of ConditionalStatements for if example.

---
+++ ConditionalStatements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+	class IfStatement
+	{
+		public static void Main (string[] args)
+		{
+      int number;
+
+			Console.WriteLine ("Please enter a number between 0 and 10:");
+      number = int.Parse (Console.ReadLine ());
+
+      if (number > 10)
+        Console.WriteLine ("Hey! The number should be 10 or less.");
+      else if (number < 0)
+        Console.WriteLine ("Hey! The number should 0 or greater.");
+        else
+          Console.WriteLine("Nice work.");
+		}
+	}
+}
