4b4df3a8c328473f700a78d84910511f7df47185
Milam <charles.milam@gmail.com>
2015-05-19 21:53:00 -0500

    Basic math example, add two numbers and display to console.

--- BasicMath/Program.cs
+++ BasicMath/Program.cs
@@ -6,7 +6,15 @@ namespace BasicMath
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+      int number1, number2;
+
+			Console.WriteLine ("Please enter a number:");
+      number1 = int.Parse (Console.ReadLine ());
+
+      Console.WriteLine ("Thanks. May I have one more:");
+      number2 = int.Parse (Console.ReadLine ());
+
+      Console.WriteLine ("Adding the numbers equals: " + (number1 + number2));
 		}
 	}
 }
