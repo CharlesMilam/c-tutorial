19bc1d6ad3567507a56674ee3c5332f4ed22335f
Milam <charles.milam@gmail.com>
2015-05-20 21:28:40 -0500

    Add do loop.

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -8,11 +8,21 @@ namespace Loops
     {
       int number = 0;
 
+      // while loop
       while(number < 5)
       {
         Console.WriteLine (number);
         number = number + 1;
       }
+
+      // reset number variable
+      number = 0;
+
+      // do loop
+      do {
+        Console.WriteLine (number);
+        number = number + 1;
+      } while(number < 5);
 
     }
   }
