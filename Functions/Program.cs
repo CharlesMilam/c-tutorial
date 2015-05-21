071df005a886a016ff5f9c39d051977f8d4fc6bd
Milam <charles.milam@gmail.com>
2015-05-20 22:24:31 -0500

    Add basic addition function.

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -7,12 +7,19 @@ namespace Functions
     public static void Main (string[] args)
     {
       DoStuff();
+      Console.WriteLine(AddNumbers (3, 6));
     }
 
     // basic function
     public static void DoStuff ()
     {
       Console.WriteLine ("I'm doing something...");
+    }
+
+    // basic addition function
+    public static int AddNumbers (int num1, int num2)
+    {
+      return num1 + num2;
     }
   }
 }
