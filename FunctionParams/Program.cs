7b5fc598400cfd7dbda8863a454630dcda5930d6
Milam <charles.milam@gmail.com>
2015-05-20 23:00:22 -0500

    Add function GreetPersons to demonstrate params.

--- FunctionParams/Program.cs
+++ FunctionParams/Program.cs
@@ -11,13 +11,24 @@ namespace FunctionParams
       AddFive (ref number);
 
       Console.WriteLine (number);
-      //Console.ReadKey ();
+
+      GreetPersons (0);
+      GreetPersons (3, "Charles", "Grace", "Alli");
     }
 
-    // add five to param
+    // add five to param, by ref example
     public static void AddFive (ref int number)
     {
       number += 5;
+    }
+
+    // params example
+    public static void GreetPersons (int unusedParam, params string[] names)
+    {
+      foreach(string name in names)
+      {
+        Console.WriteLine ("Howdy, " + name + "!");
+      }
     }
   }
 }
