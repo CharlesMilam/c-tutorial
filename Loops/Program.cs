50a2a2821957c01d9a41aec3323f5a33c07f8c2d
Milam <charles.milam@gmail.com>
2015-05-20 21:24:00 -0500

    Add basic while loop.

---
+++ Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Loops
+{
+  class MainClass
+  {
+    public static void Main (string[] args)
+    {
+      int number = 0;
+
+      while(number < 5)
+      {
+        Console.WriteLine (number);
+        number = number + 1;
+      }
+
+    }
+  }
+}
