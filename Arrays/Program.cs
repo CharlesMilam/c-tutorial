30b35313617a1ed9f3fa01d20011bae6b0f4a919
Milam <charles.milam@gmail.com>
2015-05-20 23:29:09 -0500

    Add loops for and foreach for iterating through simple arrays.

---
+++ Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arrays
+{
+  class MainClass
+  {
+    public static void Main (string[] args)
+    {
+      string[] names = new string[2];
+
+      names [0] = "Charles";
+      names [1] = "Susan";
+
+      // use foreach loop through array
+      foreach(string name in names)
+      {
+        Console.WriteLine (name);
+      }
+
+      // use for loop
+      for(int i = 0; i < names.Length; i++)
+      {
+        Console.WriteLine (i + ". " + names [i]);
+      }
+    }
+  }
+}
