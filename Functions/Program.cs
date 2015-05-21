2ca6ffe52cbe3c4b38b05097d23d41105331f409
Milam <charles.milam@gmail.com>
2015-05-20 22:13:32 -0500

    Add basic function to Functions. Add userprefs to Loops.

---
+++ Functions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Functions
+{
+  class MainClass
+  {
+    public static void Main (string[] args)
+    {
+      DoStuff();
+    }
+
+    // basic function
+    public static void DoStuff ()
+    {
+      Console.WriteLine ("I'm doing something...");
+    }
+  }
+}
